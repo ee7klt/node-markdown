ec6711cd6727d65e50257ecb4fe77181a77baf23
KL Tah <kltah1@gmail.com>
2017-03-19 09:29:43 +0800

    remove substring truncation

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.use(express.static("public"));
 
 const mongoose = require('mongoose');
 const uri = "mongodb://mongoadmin:quark8751@cluster0-shard-00-00-abnxv.mongodb.net:27017,cluster0-shard-00-01-abnxv.mongodb.net:27017,cluster0-shard-00-02-abnxv.mongodb.net:27017/markdownDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
+//const uri = "mongodb://localhost/markdownDB"
+
 mongoose.connect(uri);
 const markdownSchema = new mongoose.Schema({
   title: String,
